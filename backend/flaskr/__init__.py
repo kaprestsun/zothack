92f4f6cdc22988bb63984e5a9e001f17cca97d15
EJ Lee <eunjael1@uci.edu>
2024-11-02 13:22:48 -0700

    msgg

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,10 +1,9 @@
 import os
-
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
+#from School import Event, Student
 
 
 def create_app(test_config=None):
-    # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY='dev',
@@ -12,21 +11,30 @@ def create_app(test_config=None):
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/addevent")
+    def create_event():
+        return render_template("insert the html page with add event")
+
+    @app.route("/submit", methods=["POST"])
+    def submit_event():
+        name = request.form.get("eventName")
+        location = request.form.get("eventLocation")
+        school_class = request.form.get("eventClass")
+        professor = request.form.get("eventProfessor")
+        major = request.form.get("Major")
+        hours = request.form.get("Hours")
+
 
     return app
