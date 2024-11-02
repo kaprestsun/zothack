3ae241ea1278a6eb0cb618d6a8b71d7f2c1e3b4a
Christiana Cho <choch4@uci.edu>
2024-11-02 12:50:29 -0700

    Moved everything to init.py

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -1,7 +1,7 @@
 import os
 
-from flask import Flask
-
+from flask import Flask, jsonify, render_template, request
+from School import Event, Student
 
 def create_app(test_config=None):
     # create and configure the app
@@ -25,8 +25,22 @@ def create_app(test_config=None):
         pass
 
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.route("/")
+    def home():
+        return render_template("index.html")
+
+    @app.route("/addevent")
+    def create_event():
+        return render_template("insert the html page with add event")
+
+    @app.route("/submit", methods=["POST"])
+    def submit_event():
+        name = request.form.get("eventName")
+        location = request.form.get("eventLocation")
+        school_class = request.form.get("eventClass")
+        professor = request.form.get("eventProfessor")
+        major = request.form.get("Major")
+        hours = request.form.get("Hours")
+
 
     return app
